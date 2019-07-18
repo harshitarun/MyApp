35d7e9704eb7a9b4af6817bbe2c36060e9e72a3a
Arun, Shetty Harshit (US - Bengaluru) <sharun@deloitte.com>
2019-07-19 01:06:07 +0530

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Arun, Shetty Harshit (US - Bengaluru) on 19/07/19.
+//  Copyright © 2019 Arun, Shetty Harshit (US - Bengaluru). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
